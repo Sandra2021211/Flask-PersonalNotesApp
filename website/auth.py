b9e525e9aee4f8e2f7a6e0c4e363e0f90d8196f1
SandraDoss <200701211@rajalakshmi.edu.in>
2025-07-24 21:02:47 +0530

    Message Flashing

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,flash,redirect
 
 auth=Blueprint('auth', __name__)
 
@@ -19,16 +19,16 @@ def sign_up():
         password2=request.form['password2']
 
         if len(email)<4:
-            pass
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(firstName)<2:
-            pass
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            pass
+            flash('Passwords don\'t match.', category='error')
         elif len(password1)<7:
-            pass
+            flash('Passwords must be atleast 7 characters.', category='error')
         else:
-            #add user to db
-            pass
+            flash('Account created!', category='success')
+        return redirect(request.url)   # PRG(Post-Redirect-Get)- without redirect, other flash messages will not flash out properly
 
 
     return render_template("sign_up.html")
